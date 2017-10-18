6526ed166bb6a36b5f08e88ca573b552cee1ae08
David Grove <dgrove-oss@users.noreply.github.com>
2017-10-18 12:02:10 -0400

    Fix travis builds by combining PR70 and PR71 (#72)
    * disable use of docker-runc by setting INVOKER_USE_RUNC to false
    
    Set envvar to disable use of docker-runc to pause/resume containers
    (will use docker pause/unpause instead).
    
    * define invoker envvars now required by OW PR#2689
    
    * move package update/install from init.sh into Dockerfile and force image pull

--- kubernetes/couchdb/docker/init.sh
+++ kubernetes/couchdb/docker/init.sh
@@ -5,15 +5,6 @@ set -ex
 git clone https://github.com/apache/incubator-openwhisk /openwhisk
 
 pushd /openwhisk
-  # Install ansible requirements
-  ./tools/ubuntu-setup/pip.sh
-
-  # upgrade cffi for ansible error on Debian Jesse
-  pip install --upgrade cffi
-  sudo pip install markupsafe
-  sudo pip install ansible==2.3.0.0
-
-
   # if auth guest overwrite file
   if [ -n "$AUTH_GUEST" ]; then
     echo "$AUTH_GUEST" > /openwhisk/ansible/files/auth.guest
