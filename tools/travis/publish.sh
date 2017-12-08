5a4d547622e7d58969fe0f4fdbd0ad360c6e7b09
David Grove <groved@us.ibm.com>
2017-12-08 11:57:58 -0500

    reorg docker files to enable publishing multiple official images
    Reorganize to pull all docker subdirs out from under kubernetes
    directory to a new top-level docker directory.
    
    Tweak publish script to take the directory to build as a parameter
    to enable us to build and publish multiple images (coming soon...).

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -4,8 +4,9 @@ set -exu
 dockerhub_image_prefix="$1"
 dockerhub_image_name="$2"
 dockerhub_image_tag="$3"
+dir_to_build="$4"
 dockerhub_image="${dockerhub_image_prefix}/${dockerhub_image_name}:${dockerhub_image_tag}"
 
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-docker build kubernetes/couchdb/docker --tag ${dockerhub_image}
+docker build ${dir_to_build} --tag ${dockerhub_image}
 docker push ${dockerhub_image}
