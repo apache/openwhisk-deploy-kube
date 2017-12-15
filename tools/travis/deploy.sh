apache/openwhisk-deploy-kube

commit 9f8e75d7ac221c6314ad9bc2d2547baa9306ddf8

add cluster-setup step to deployment workflow and Job to install routemgmt actions (#123)
* add cluster-setup step to deployment workflow

Reorganize to create additional config maps and secrets
during a cluster-setup phase to simplify later steps and
enable defining a post-deploy Job to install routemgt,
package catalog, etc.

* support routemgmt installation for apigateway

Add support for running a Job to deploy the routemgmt functions
in support of apigateway functionality.  The functionality is not
mentioned in the toplevel README.md or in the travis build script
yet because we first need a successful travis build on master to
publish the new openwhisk/kube-routemgmt image to dockerhub.

.travis.yml
@@ -24,5 +24,6 @@ deploy:
 - provider: script
   script:
     - ./tools/travis/publish.sh openwhisk kube-couchdb latest docker/couchdb
+    - ./tools/travis/publish.sh openwhisk kube-routemgmt latest docker/routemgmt
   on:
     branch: master

docker/routemgmt/Dockerfile
@@ -1,0 +1,17 @@
+from ubuntu:latest
+
+RUN apt-get -y update && apt-get -y install \
+  git \
+  wget \
+  zip \
+  python-dev \
+  python-pip
+
+RUN pip install --upgrade setuptools
+RUN pip install argcomplete
+RUN pip install ansible==2.3.0.0
+
+COPY init.sh /init.sh
+RUN chmod +X /init.sh
+
+CMD ["/init.sh"]

docker/routemgmt/init.sh
@@ -1,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+export OPENWHISK_HOME=/openwhisk
+
+# Clone openwhisk repo to get latest installRouteMgmt.sh and core/routemgmt
+# TODO: when OpenWhisk has releases, download release artifacts instead!
+git clone https://github.com/apache/incubator-openwhisk openwhisk
+
+cd $OPENWHISK_HOME
+
+# Download and install openwhisk cli
+pushd bin
+  wget -q https://github.com/apache/incubator-openwhisk-cli/releases/download/$WHISK_CLI_VERSION/OpenWhisk_CLI-$WHISK_CLI_VERSION-linux-amd64.tgz
+  tar xzf OpenWhisk_CLI-$WHISK_CLI_VERSION-linux-amd64.tgz
+popd
+
+# Generate whisk.properties.
+# TODO: Refactor upstream ansible/roles/routemgmt/files/installRouteMgmt.sh to enable
+# override of apigw values from environment so we don't have to bother running
+# ansible here to generate whisk.properties just so the script can extract 3 values.
+pushd ansible
+  ansible-playbook setup.yml
+  ansible-playbook properties.yml -e apigw_host_v2=$WHISK_API_GATEWAY_HOST_V2
+popd
+
+# Run installRouteMgmt.sh
+pushd ansible/roles/routemgmt/files
+  ./installRouteMgmt.sh $WHISK_AUTH $WHISK_API_HOST_NAME $WHISK_NAMESPACE $OPENWHISK_HOME/bin/wsk
+popd
+

kubernetes/cluster-setup/auth.guest
@@ -1,0 +1,1 @@
+23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP

kubernetes/cluster-setup/auth.whisk.system
@@ -1,0 +1,1 @@
+789c46b1-71f6-4ed5-8c54-816aa4f8c502:abczO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP

kubernetes/cluster-setup/whisk.env
@@ -1,0 +1,2 @@
+# External hostname used to access your OpenWhisk deployment
+whisk_api_host_name=localhost

kubernetes/invoker/invoker.yml
@@ -59,7 +59,10 @@ spec:
           - name: "SERVICE_CHECK_INTERVAL"
             value: "15s"
           - name: "WHISK_API_HOST_NAME"
-            value: "nginx.openwhisk"
+            valueFrom:
+              configMapKeyRef:
+                name: whisk
+                key: whisk_api_host_name
           - name: "WHISK_VERSION_BUILDNO"
             value: "latest"
           - name: "INVOKER_CONTAINER_NETWORK"
@@ -74,16 +77,6 @@ spec:
             value: "latest"
           - name: "DOCKER_REGISTRY"
             value: ""
-
-          # Invoker instance count. Needs to match replica count
-          - name: "INVOKER_INSTANCES"
-            value: "1"
-
-          # Invoker assigned name. Derived from hostname
-          - name: "INVOKER_NAME"
-            valueFrom:
-              fieldRef:
-                fieldPath: metadata.name
 
           # Java options
           - name: "JAVA_OPTS"
@@ -105,7 +98,7 @@ spec:
           - name: "RUNTIMES_MANIFEST"
             value: '{ "defaultImagePrefix": "openwhisk", "defaultImageTag": "latest", "runtimes": { "nodejs": [ { "kind": "nodejs", "image": { "name": "nodejsaction" }, "deprecated": true }, { "kind": "nodejs:6", "default": true, "image": { "name": "nodejs6action" }, "deprecated": false } ], "python": [ { "kind": "python", "image": { "name": "python2action" }, "deprecated": false }, { "kind": "python:2", "default": true, "image": { "name": "python2action" }, "deprecated": false }, { "kind": "python:3", "image": { "name": "python3action" }, "deprecated": false } ], "swift": [ { "kind": "swift", "image": { "name": "swiftaction" }, "deprecated": true }, { "kind": "swift:3", "image": { "name": "swift3action" }, "deprecated": true }, { "kind": "swift:3.1.1", "default": true, "image": { "name": "action-swift-v3.1.1" }, "deprecated": false } ], "java": [ { "kind": "java", "default": true, "image": { "name": "java8action" }, "deprecated": false, "attached": { "attachmentName": "jarfile", "attachmentType": "application/java-archive" }, "sentinelledLogs": false, "requireMain": true } ] }, "blackboxes": [ { "name": "dockerskeleton" } ] }'
 
-          # Default to empty logs dir. This is because logs should go to stdout
+          # Default to empty logs dir. This is because logs should go to stdout on kube
           - name: "WHISK_LOGS_DIR"
             value: ""
 

commit dbf9fae1faa9edb9e9ae971d74921d155d4feebf

build fix: Travis deploy step must be a single script
Fix bug introduced in 9f8e75d7 -- didn't read the Travis CI docs
closely enough.  The deploy step with the script provider
must be a single script.

.travis.yml
@@ -22,8 +22,6 @@ script:
 
 deploy:
 - provider: script
-  script:
-    - ./tools/travis/publish.sh openwhisk kube-couchdb latest docker/couchdb
-    - ./tools/travis/publish.sh openwhisk kube-routemgmt latest docker/routemgmt
+  script: ./tools/travis/deploy.sh
   on:
     branch: master

tools/travis/deploy.sh
@@ -1,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../../"
+
+cd $ROOTDIR
+
+echo "Publishing kube-couchdb image"
+./tools/travis/publish.sh openwhisk kube-couchdb latest docker/couchdb
+
+echo "Publishing kube-routemgmt image"
+./tools/travis/publish.sh openwhisk kube-routemgmt latest docker/routemgmt