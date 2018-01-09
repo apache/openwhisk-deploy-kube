05f9732aa7d131f4eca6b57bf94b5a4ea92796e6
David Grove <dgrove-oss@users.noreply.github.com>
2018-01-09 13:04:20 -0500

    use runtimesManifest to drive invoker 'docker pull' (#140)
    Replace hard-wired list of images to pull from invoker.yml
    by an initContainer that interprets runtimesManifest to
    determine which images to pull before starting the main
    invoker container.
    
    Fixes #135

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -10,8 +10,11 @@ cd $ROOTDIR
 echo "Publishing kube-couchdb image"
 ./tools/travis/publish.sh openwhisk kube-couchdb latest docker/couchdb
 
-echo "Publishing kube-routemgmt image"
-./tools/travis/publish.sh openwhisk kube-routemgmt latest docker/routemgmt
+echo "Publishing kube-docker-pull image"
+./tools/travis/publish.sh openwhisk kube-docker-pull latest docker/docker-pull
 
 echo "Publishing kube-openwhisk-catalog image"
 ./tools/travis/publish.sh openwhisk kube-openwhisk-catalog latest docker/openwhisk-catalog
+
+echo "Publishing kube-routemgmt image"
+./tools/travis/publish.sh openwhisk kube-routemgmt latest docker/routemgmt
