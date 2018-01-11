apache/openwhisk-deploy-kube

commit 84f6e38d66cdea5342cbc933a852a72ca6f719d2

bugfix for setting WSK_HOST on minikube (#143)
When minikube isn't using the none driver, the Hostname
returned from `kubectl describe nodes` may not be resolvable.
Use `minikube ip` instead to get an ip addr for WSK_HOST.

Also update the logs command that runs when the script fails
to use the name=invoker label (missed when we converted the
invoker from statefulsets to daemonsets).

tools/travis/build.sh
@@ -158,7 +158,7 @@ popd
 pushd kubernetes/ingress
   WSK_PORT=$(kubectl -n openwhisk describe service nginx | grep https-api | grep NodePort| awk '{print $3}' | cut -d'/' -f1)
   APIGW_PORT=$(kubectl -n openwhisk describe service apigateway | grep mgmt | grep NodePort| awk '{print $3}' | cut -d'/' -f1)
-  WSK_HOST=$(kubectl describe nodes | grep Hostname: | awk '{print $2}')
+  WSK_HOST=$(minikube ip)
   kubectl -n openwhisk create configmap whisk.ingress --from-literal=api_host=$WSK_HOST:$WSK_PORT --from-literal=apigw_url=http://$WSK_HOST:$APIGW_PORT
   wsk property set --auth `cat ../cluster-setup/auth.guest` --apihost $WSK_HOST:$WSK_PORT
 popd
@@ -277,7 +277,7 @@ if [ -z "$RESULT" ]; then
   kubectl -n openwhisk logs controller-0
 
   echo " ----------------------------- invoker logs ---------------------------"
-  kubectl -n openwhisk logs invoker-0
+  kubectl -n openwhisk logs -l name=invoker
   exit 1
 fi
 

commit 9c4428e6a81436ed7072154656f527245682df70

fix Travis CI regression introduced by 84f6e38d.
Only use `minikube ip` to get WSK_HOST when `kubectl describe nodes`
returns "minikube" as the hostname.  When running with the none driver
(like in travis CI), minikube ip returns local host, which works
for the wsk CLI externally, but not for the wsk CLI when invoked
from a Job running within minikube.

Regression was not detected during PR testing because the fix
of 63f96636 to check error conditions in the job's init.sh has not
been merged to master when 84f6e38d was tested.

tools/travis/build.sh
@@ -158,7 +158,10 @@ popd
 pushd kubernetes/ingress
   WSK_PORT=$(kubectl -n openwhisk describe service nginx | grep https-api | grep NodePort| awk '{print $3}' | cut -d'/' -f1)
   APIGW_PORT=$(kubectl -n openwhisk describe service apigateway | grep mgmt | grep NodePort| awk '{print $3}' | cut -d'/' -f1)
-  WSK_HOST=$(minikube ip)
+  WSK_HOST=$(kubectl describe nodes | grep Hostname: | awk '{print $2}')
+  if [ "$WSK_HOST" = "minikube" ]; then
+      WSK_HOST=$(minikube ip)
+  fi
   kubectl -n openwhisk create configmap whisk.ingress --from-literal=api_host=$WSK_HOST:$WSK_PORT --from-literal=apigw_url=http://$WSK_HOST:$APIGW_PORT
   wsk property set --auth `cat ../cluster-setup/auth.guest` --apihost $WSK_HOST:$WSK_PORT
 popd