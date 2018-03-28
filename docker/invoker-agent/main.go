9815bd8bd8c0a19d30a71e1bdf02a7227c712278
David Grove <dgrove-oss@users.noreply.github.com>
2018-03-28 11:09:27 -0400

    config files to use kubernetes container pool and invoker-agent (#155)
    1. Deployment files for invoker statefulset using KubernetesContainerPool.
    2. Update invoker configuration to describe the two container factory options
    3. Implement simple Go invoker-agent to proxy pause/unpause operations
       and log consolidation for a remote invoker instance.

---
+++ docker/invoker-agent/main.go
@@ -0,0 +1,336 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+/* JSON structure expected as request body on /logs route */
+type LogForwardInfo struct {
+	LastOffset             int64  `json:"lastOffset"`               // last offset read from this container's log
+	SizeLimit              int    `json:"sizeLimit"`                // size limit on logs read in bytes
+	SentinelledLogs        bool   `json:"sentinelledLogs"`          // does an action's log end with sentinel lines?
+	EncodedLogLineMetadata string `json:"encodedLogLineMetadata"`   // string to be injected in every log line
+	EncodedActivation      string `json:"encodedActivation"`        // extra line to injected after all log lines are read
+}
+
+/* Size threshold for individual output files written by the logWriter */
+
+/* String constants related to logging */
+const (
+	logSentinelLine        = "XXX_THE_END_OF_A_WHISK_ACTIVATION_XXX"
+	truncatedLogMessage    = "Logs were truncated because the total bytes size exceeds the limit of %d bytes."
+	genericLogErrorMessage = "There was an issue while collecting your logs. Data might be missing."
+)
+
+/* Should we measure and report time taken for each operation? */
+const timeOps = false
+
+/* configuration variables; may be overridden by setting matching envvar */
+var (
+	dockerSock       string = "/var/run/docker.sock"
+	containerDir     string = "/containers"
+	outputLogDir     string = "/action-logs"
+	invokerAgentPort int    = 3233
+	logSinkSize      int64  = 100 * 1024 * 1024
+)
+
+/* http.Client instance bound to dockerSock */
+var client *http.Client
+
+/* channel to send log lines to the logWriter */
+var logSinkChannel chan string
+
+/*
+ * Support for writing log lines to the logSink
+ */
+
+// go routine that accepts log lines from the logSinkChannel and writes them to the logSink
+func logWriter() {
+	var sinkFile *os.File = nil
+	var sinkFileBytes int64 = 0
+	var err error
+
+	for {
+		line := <-logSinkChannel
+
+		if sinkFile == nil {
+			timestamp := time.Now().UnixNano() / 1000000
+			fname := fmt.Sprintf("%s/userlogs-%d.log", outputLogDir, timestamp)
+			sinkFile, err = os.Create(fname)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Unable to create log sink: %v\n", err)
+				panic(err)
+			}
+			sinkFileBytes = 0
+		}
+
+		bytesWritten, err := fmt.Fprintln(sinkFile, line)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing to log sink: %v\n", err)
+			panic(err)
+		}
+
+		sinkFileBytes += int64(bytesWritten)
+		if sinkFileBytes > logSinkSize {
+			sinkFile.Close()
+			sinkFile = nil
+		}
+	}
+}
+
+func writeSyntheticLogLine(msg string, metadata string) {
+	now := time.Now().UTC().Format(time.RFC3339)
+	line := fmt.Sprintf("{\"log\":\"%s\", \"stream\":\"stderr\", \"time\":\"%s\",%s}", msg, now, metadata)
+	logSinkChannel <- line
+}
+
+func reportLoggingError(w http.ResponseWriter, code int, msg string, metadata string) {
+	w.WriteHeader(code)
+	fmt.Fprint(w, msg)
+	fmt.Fprintln(os.Stderr, msg)
+	if metadata != "" {
+		writeSyntheticLogLine(genericLogErrorMessage, metadata)
+	}
+}
+
+// Request handler for /logs/<container> route
+// The container was given as part of the URL; gorilla makes it available in vars["container"]
+// The JSON body of the request is expected to contain the fields specified by the
+// LogForwardInfo struct defined above.
+// If logs are successfully forwarded, the ending offset of the log file is returned
+// to be used in a subsequent call to the /logs/<container> route.
+func forwardLogsFromUserAction(w http.ResponseWriter, r *http.Request) {
+	var start time.Time
+	if timeOps {
+		start = time.Now()
+	}
+
+	vars := mux.Vars(r)
+	container := vars["container"]
+
+	var lfi LogForwardInfo
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		reportLoggingError(w, 400, fmt.Sprintf("Error reading request body: %v", err), "")
+		return
+	}
+	err = json.Unmarshal(b, &lfi)
+	if err != nil {
+		reportLoggingError(w, 400, fmt.Sprint("Error unmarshalling request body: %v", err), "")
+		return
+	}
+
+	logFileName := containerDir + "/" + container + "/" + container + "-json.log"
+	logFile, err := os.Open(logFileName)
+	defer logFile.Close()
+	if err != nil {
+		reportLoggingError(w, 500, fmt.Sprintf("Error opening %s: %v", logFileName, err), lfi.EncodedLogLineMetadata)
+		logSinkChannel <- lfi.EncodedActivation // Write activation record before returning with error code.
+		return
+	}
+
+	offset, err := logFile.Seek(lfi.LastOffset, 0)
+	if offset != lfi.LastOffset || err != nil {
+		reportLoggingError(w, 500, fmt.Sprintf("Unable to seek to %d in log file", lfi.LastOffset), lfi.EncodedLogLineMetadata)
+		logSinkChannel <- lfi.EncodedActivation // Write activation record before returning with error code.
+		return
+	}
+
+	sentinelsLeft := 2
+	scanner := bufio.NewScanner(logFile)
+	bytesWritten := 0
+	for sentinelsLeft > 0 && scanner.Scan() {
+		logLine := scanner.Text()
+		if lfi.SentinelledLogs && strings.Contains(logLine, logSentinelLine) {
+			sentinelsLeft -= 1
+		} else {
+			logLineLen := len(logLine)
+			bytesWritten += logLineLen
+			mungedLine := fmt.Sprintf("%s,%s}", logLine[:logLineLen-1], lfi.EncodedLogLineMetadata)
+			logSinkChannel <- mungedLine
+			if bytesWritten > lfi.SizeLimit {
+				writeSyntheticLogLine(fmt.Sprintf(truncatedLogMessage, lfi.SizeLimit), lfi.EncodedLogLineMetadata)
+				logFile.Seek(0, 2) // Seek to end of logfile to skip rest of output and prepare for next action invoke
+				sentinelsLeft = 0  // Cause loop to exit now.
+			}
+		}
+	}
+
+	if lfi.SentinelledLogs && sentinelsLeft != 0 {
+		reportLoggingError(w, 500, "Failed to find expected sentinels in log file", lfi.EncodedLogLineMetadata)
+		logSinkChannel <- lfi.EncodedActivation // Write activation record before returning with error code.
+		return
+	}
+
+	// Done copying log; write the activation record.
+	logSinkChannel <- lfi.EncodedActivation
+
+	// seek 0 bytes from current position to set logFileOffset to current fpos
+	logFileOffset, err := logFile.Seek(0, 1)
+	if err != nil {
+		reportLoggingError(w, 500, fmt.Sprintf("Unable to determine current offset in log file: %v", err), lfi.EncodedLogLineMetadata)
+		return
+	}
+
+	// Success; return updated logFileOffset to invoker
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "%d", logFileOffset)
+
+	if timeOps {
+		end := time.Now()
+		elapsed := end.Sub(start)
+		fmt.Fprintf(os.Stdout, "LogForward took %s\n", elapsed.String())
+	}
+}
+
+/*
+ * Suppout for suspend/resume operations
+ */
+
+// handler for /resume/<container> route
+// The container was given as part of the URL; gorilla makes it available in vars["container"]
+func resumeUserAction(w http.ResponseWriter, r *http.Request) {
+	var start time.Time
+	if timeOps {
+		start = time.Now()
+	}
+
+	vars := mux.Vars(r)
+	container := vars["container"]
+	dummy := strings.NewReader("")
+	resp, err := client.Post("http://localhost/containers/"+container+"/unpause", "text/plain", dummy)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Unpausing %s failed with error: %v\n", container, err)
+	} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Unpausing %s failed with status code: %d\n", container, resp.StatusCode)
+	} else {
+		w.WriteHeader(204) // success!
+	}
+
+	if timeOps {
+		end := time.Now()
+		elapsed := end.Sub(start)
+		fmt.Fprintf(os.Stdout, "Unpause took %s\n", elapsed.String())
+	}
+}
+
+// handler for /resume/<container> route
+// The container was given as part of the URL; gorilla makes it available in vars["container"]
+func suspendUserAction(w http.ResponseWriter, r *http.Request) {
+	var start time.Time
+	if timeOps {
+		start = time.Now()
+	}
+
+	vars := mux.Vars(r)
+	container := vars["container"]
+	dummy := strings.NewReader("")
+	resp, err := client.Post("http://localhost/containers/"+container+"/pause", "text/plain", dummy)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Pausing %s failed with error: %v\n", container, err)
+	} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Pausing %s failed with status code: %d\n", container, resp.StatusCode)
+	} else {
+		w.WriteHeader(204) // success!
+	}
+
+	if timeOps {
+		end := time.Now()
+		elapsed := end.Sub(start)
+		fmt.Fprintf(os.Stdout, "Pause took %s\n", elapsed.String())
+	}
+}
+
+/*
+ * Initialization and main function
+ */
+
+// Process configuration overrides from environment
+func initializeFromEnv() {
+	var err error
+	if os.Getenv("INVOKER_AGENT_DOCKER_SOCK") != "" {
+		dockerSock = os.Getenv("INVOKER_AGENT_DOCKER_SOCK")
+	}
+	if os.Getenv("INVOKER_AGENT_CONTAINER_DIR") != "" {
+		containerDir = os.Getenv("INVOKER_AGENT_CONTAINER_DIR")
+	}
+	if os.Getenv("INVOKER_AGENT_OUTPUT_LOG_DIR") != "" {
+		outputLogDir = os.Getenv("INVOKER_AGENT_OUTPUT_LOG_DIR")
+	}
+	if os.Getenv("INVOKER_AGENT_PORT") != "" {
+		str := os.Getenv("INVOKER_AGENT_PORT")
+		invokerAgentPort, err = strconv.Atoi(str)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid INVOKER_AGENT_PORT %s; error was %v\n", str, err)
+			panic(err)
+		}
+	}
+	if os.Getenv("INVOKER_AGENT_LOG_SINK_SIZE") != "" {
+		str := os.Getenv("INVOKER_AGENT_LOG_SINK_SIZE")
+		logSinkSize, err = strconv.ParseInt(str, 10, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid INVOKER_AGENT_LOG_SINK_SIZE %s; error was %v\n", str, err)
+			panic(err)
+		}
+	}
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/logs/{container}", forwardLogsFromUserAction)
+	myRouter.HandleFunc("/suspend/{container}", suspendUserAction)
+	myRouter.HandleFunc("/resume/{container}", resumeUserAction)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", invokerAgentPort), myRouter))
+}
+
+func main() {
+	initializeFromEnv()
+
+	// Open http client to dockerSock
+	fd := func(proto, addr string) (conn net.Conn, err error) {
+		return net.Dial("unix", dockerSock)
+	}
+	tr := &http.Transport{
+		Dial: fd,
+	}
+	client = &http.Client{Transport: tr}
+
+	// initialize logSink subsystem & schedule logWrite go routine
+	logSinkChannel = make(chan string)
+	go logWriter()
+
+	handleRequests()
+}
--- kubernetes/invoker/README.md
+++ kubernetes/invoker/README.md
@@ -1,45 +1,109 @@
 Invoker
 -------
 
+# Overview
+
+The Invoker is responsible for creating and managing the containers
+that OpenWhisk creates to execute the user defined functions.  A key
+function of the Invoker is to manage a cache of available warm
+containers to minimize cold starts of user functions.
+Architecturally, we support two options for deploying the Invoker
+component on Kubernetes (selected by picking a
+`ContainerFactoryProviderSPI` for your deployment).
+  1. `DockerContainerFactory` matches the architecture used by the
+      non-Kubernetes deployments of OpenWhisk.  In this approach, an
+      Invoker instance runs on every Kubernetes worker node that is
+      being used to execute user functions.  The Invoker directly
+      communicates with the docker daemon running on the worker node
+      to create and manage the user function containers.  The primary
+      advantages of this configuration are lower latency on container
+      management operations and robustness of the code paths being
+      used (since they are the same as in the default system).  The
+      primary disadvantage is that it does not leverage Kubernetes to
+      simplify resource management, security configuration, etc. for
+      user containers.
+  2. `KubernetesContainerFactory` is a truly Kubernetes-native design
+      where although the Invoker is still responsible for managing the
+      cache of available user containers, the Invoker relies on Kubernetes to
+      create, schedule, and manage the Pods that contain the user function
+      containers. The pros and cons of this design are roughly the
+      inverse of `DockerContainerFactory`.  Kubernetes pod management
+      operations have higher latency and exercise newer code paths in
+      the Invoker.  However, this design fully leverages Kubernetes to
+      manage the execution resources for user functions.
+
 # Deploying
 
-## Create config map
+## Label the worker nodes
 
-Edit invoker.env as needed to set the appropriate values for your
-deployment, then create the configmap invoker.config:
-
+In either approach, it is desirable to indicate which worker nodes
+should be used to execute user containers.  Do this by labeling each
+node with `openwhisk-role=invoker`.  For a single node cluster, simply do
 ```
-kubectl -n openwhisk create cm invoker.config --from-env-file=invoker.env
+kubectl label nodes --all openwhisk-role=invoker
 ```
-
-## Deploy Invoker
-
-When deploying the Invoker, it needs to be deployed via a
-[DaemonSet](https://kubernetes.io/docs/concepts/workloads/controllers/daemonset/).
-This is because there should only ever be at most 1 Invoker
-instance per Kube Node. To set these restrictions, it will be
-up to the Kubernetes deployment operator to properly apply
-the correct labels and taints to each required Kube node.
-
-With the defaults in the current `invoker.yml`, you can setup a
-node to run only Invoker pods with:
-
+If you have a multi-node cluster, for each node <INVOKER_NODE_NAME>
+you want to be an invoker, execute
 ```
-kubectl label nodes [node name] openwhisk-role=invoker
-$ kubectl label nodes 127.0.0.1 openwhisk-role=invoker
+$ kubectl label nodes <INVOKER_NODE_NAME> openwhisk-role=invoker
 ```
 
-Once the invoker label is applied, you can create the invokers with:
+## Deploying using the DockerContainerFactory
 
+### Create the invoker.config config map
+
+Edit invoker-dcf.env to make any customizations needed for your
+deployment, create the config map:
 ```
-kubectl apply -f invoker.yml
+kubectl -n openwhisk create cm invoker.config --from-env-file=invoker-dcf.env
 ```
 
-**Important**
+### Deploy the Invoker as a DaemonSet
+
+This will deploy an Invoker instance on every Kubernetes worker node
+labeled with openwhisk-role=invoker.
+```
+kubectl apply -f invoker-dcf.yml
+```
+
+## Deploying using the KubernetesContainerFactory
+
+The KubernetesContainerFactory can be deployed with an additional
+invokerAgent that implements container suspend/resume operations on
+behalf of a remote Invoker.  The instructions here included deploying
+the invokerAgent.  If you do not want to do this, skip deploying the
+invokerAgent daemonset and edit invoker-k8scf.yml to set
+`CONFIG_whisk_kubernetes_invokerAgent_enabled` to `FALSE`.
+
+### Create the invoker.config config map
+
+Edit invoker-k8scf.env to make any customizations needed for your
+deployment, create the config map:
+```
+kubectl -n openwhisk create cm invoker.config --from-env-file=invoker-k8scf.env
+```
+
+### Deploy the invokerAgent Daemonset
+```
+kubectl apply -f invoker-agent.yml
+```
+Wait for all of the invoker-agent pods to be running.  This might take a
+couple of minutes because the invoker-agent also prefetches the docker images
+for the default set of user action runtimes by doing docker pulls as an
+init container.
+
+### Deploy the Invoker as a StatefulSet
+
+By default, this will deploy a single Invoker instance.  Optionally
+edit invoker-k8scf.yml to change the number of Invoker replicas and
+then do:
+```
+kubectl apply -f invoker-k8scf.yml
+```
 
 
 # Troubleshooting
-## No invokers are deployed
+## No invokers are deployed with DockerContainerFactory
 
 Verify that you actually have at least one node with the label openwhisk-role=invoker.
 
@@ -54,5 +118,5 @@ means that the default volume hostPath values assume that the Kubernetes worker
 node image is Ubuntu. If containers fail to start with errors related
 mounting`/sys/fs/cgroup`, `/run/runc`,`/var/lib/docker/containers`, or
 `/var/run/docker.sock`, then you will need to change the corresponding
-value in [invoker.yml](invoker.yml) to match the host operating system
+value in [invoker-dcf.yml](invoker-dcf.yml) to match the host operating system
 running on your Kubernetes worker node.
