02cc71f2c328738bb932b2188e6f50db83e23346
David Grove <dgrove-oss@users.noreply.github.com>
2018-04-05 10:50:06 -0400

    reduce number of runtimes for docker-pull during TravisCI testing (#178)
    pulling all the runtime docker images takes a significant fraction
    of the overall testing time.  Just pull the two runtime images
    we actually need to execute our test cases to reduce test time.

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -148,7 +148,12 @@ pushd kubernetes/cluster-setup
   kubectl apply -f namespace.yml
   kubectl apply -f services.yml
   kubectl -n openwhisk create cm whisk.config --from-env-file=config.env
-  kubectl -n openwhisk create cm whisk.runtimes --from-file=runtimes=runtimes.json
+  if [ "$TRAVIS" = "true" ]; then
+      # when running a CI job, skip pulling images we don't need to significantly reduce testing time.
+      kubectl -n openwhisk create cm whisk.runtimes --from-file=runtimes=runtimes-minimal-travis.json
+  else
+      kubectl -n openwhisk create cm whisk.runtimes --from-file=runtimes=runtimes.json
+  fi
   kubectl -n openwhisk create cm whisk.limits --from-env-file=limits.env
   kubectl -n openwhisk create secret generic whisk.auth --from-file=system=auth.whisk.system --from-file=guest=auth.guest
 popd
