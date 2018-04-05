ac1dffeb98f92a915fd311ad29508dffa7105c62
David Grove <groved@us.ibm.com>
2018-04-05 17:41:46 -0400

    put db_host and db_port in db.config configmap
    Better support for using an external (not in kube) database by
    eliminating the assumption that there must be a couchdb service and
    instead getting the db_host and db_port from the db.config configmap.

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -175,7 +175,7 @@ popd
 echo "Deploying couchdb"
 pushd kubernetes/couchdb
   kubectl -n openwhisk create secret generic db.auth --from-literal=db_username=whisk_admin --from-literal=db_password=some_passw0rd
-  kubectl -n openwhisk create configmap db.config --from-literal=db_protocol=http --from-literal=db_provider=CouchDB --from-literal=db_whisk_activations=test_activations --from-literal=db_whisk_actions=test_whisks --from-literal=db_whisk_auths=test_subjects --from-literal=db_prefix=test_
+  kubectl -n openwhisk create configmap db.config --from-literal=db_protocol=http --from-literal=db_provider=CouchDB --from-literal=db_host=couchdb.openwhisk.svc.cluster.local --from-literal=db_port=5984 --from-literal=db_whisk_activations=test_activations --from-literal=db_whisk_actions=test_whisks --from-literal=db_whisk_auths=test_subjects --from-literal=db_prefix=test_
   kubectl apply -f couchdb.yml
 
   couchdbHealthCheck
