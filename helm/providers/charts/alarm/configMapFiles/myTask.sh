cb9f27f064132a2f47778c28806d5b1bdf0382a4
Daisy Guo <guoyingc@cn.ibm.com>
2018-07-25 19:11:27 +0800

    add alarm provider

---
+++ helm/providers/charts/alarm/configMapFiles/myTask.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more contributor
+# license agreements; and to You under the Apache License, Version 2.0.
+
+# installCatalog.sh expects the wsk cli to be $OPENWHISK_HOME/bin/wsk
+
+export OPENWHISK_HOME=/usr/local
+
+git clone https://github.com/apache/incubator-openwhisk-package-kafka.git
+
+export DB_URL=http://$DB_USERNAME:$DB_PASSWORD@$DB_HOST:$DB_PORT
+pushd /incubator-openwhisk-package-alarms
+  ./installCatalog.sh $AUTH $APIHOST $DB_URL $DB_PREFIX $APIHOST
+popd
+
+echo "successfully setup alarm package"
