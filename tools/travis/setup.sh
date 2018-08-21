d3bcd3a525154af9b6eb2a80954fd8891f072753
David Grove <groved@us.ibm.com>
2018-08-21 12:12:06 -0400

    WIP

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -2,37 +2,10 @@
 # Licensed to the Apache Software Foundation (ASF) under one or more contributor
 # license agreements; and to You under the Apache License, Version 2.0.
 
-# This script assumes Docker is already installed
 set -x
-
-#check_or_build_nsenter derived from https://github.com/bitnami/kubernetes-travis/blob/master/scripts/cluster-up-minikube.sh (Apache 2.0 License)
-check_or_build_nsenter() {
-    which nsenter >/dev/null && return 0
-    echo "INFO: Building 'nsenter' ..."
-cat <<-EOF | docker run -i --rm -v "$(pwd):/build" ubuntu:14.04 >& nsenter.build.log
-        apt-get update
-        apt-get install -qy git bison build-essential autopoint libtool automake autoconf gettext pkg-config
-        git clone --depth 1 git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git /tmp/util-linux
-        cd /tmp/util-linux
-        ./autogen.sh
-        ./configure --without-python --disable-all-programs --enable-nsenter
-        make nsenter
-        cp -pfv nsenter /build
-EOF
-    if [ ! -f ./nsenter ]; then
-        echo "ERROR: nsenter build failed, log:"
-        cat nsenter.build.log
-        return 1
-    fi
-    echo "INFO: nsenter build OK, installing ..."
-    sudo install -v nsenter /usr/local/bin
-}
 
 # Download and install misc packages and utilities
 pushd /tmp
-  # helm on minikube's vm-driver=none requires nsenter
-  check_or_build_nsenter
-
   # download and install the wsk cli
   wget -q https://github.com/apache/incubator-openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-amd64.tgz
   tar xzf OpenWhisk_CLI-latest-linux-amd64.tgz
@@ -40,12 +13,11 @@ pushd /tmp
 
   # Download and install kubectl and minikube following the recipe in the minikube
   # project README.md for using minikube for Linux Continuous Integration with VM Support
-    curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$TRAVIS_KUBE_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin
+  curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$TRAVIS_KUBE_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin
   curl -Lo minikube https://storage.googleapis.com/minikube/releases/$TRAVIS_MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin
 
   # Download and install helm
   curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh && chmod +x get_helm.sh && ./get_helm.sh
-
 popd
 
 # set docker0 to promiscuous mode
