c0ecea197649e8c99bc3b53c2d0c1c8550889956
David Grove <groved@us.ibm.com>
2020-02-26 14:55:30 -0500

    minor version bump for helm, kubectl, kind

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -18,9 +18,9 @@
 
 set -x
 
-HELM_VERSION=v3.0.1
-KIND_VERSION=v0.6.1
-KUBECTL_VERSION=v1.16.3
+HELM_VERSION=v3.0.3
+KIND_VERSION=v0.7.0
+KUBECTL_VERSION=v1.16.7
 WSK_CLI_VERSION=latest
 
 # Download and install command line tools
